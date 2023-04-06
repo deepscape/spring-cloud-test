78a6c9341cca626e2214502a35e5c4b9738d3049
deepscape <hdkim.scala@gmail.com>
2023-04-06 11:31:56 +0900

    ch10 : 시작

--- Organization Service/src/main/java/com/optimagrowth/organization/OrganizationServiceApplication.java
+++ Organization Service/src/main/java/com/optimagrowth/organization/OrganizationServiceApplication.java
@@ -3,9 +3,12 @@ package com.optimagrowth.organization;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
 @RefreshScope    // Config Server 변경되면, Refresh
+// 본 서비스가 Source Class 에 정의된 채널을 이용해 메시지 브로커와 통신할 것이라고 spring cloud stream 에 알리는 역할
+// @EnableBinding(Source.class)
 public class OrganizationServiceApplication {
 
     public static void main(String[] args) {
